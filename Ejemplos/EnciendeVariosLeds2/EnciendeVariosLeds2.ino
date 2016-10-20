0e9df5b5823ab725aefd5755fae8bafe18f51922
toblerone554 <toblerone554@gmail.com>
2016-10-20 20:02:17 +0200

    Mas ejemplos de leds

--- Ejemplos/EnciendeVariosLeds2/EnciendeVariosLeds2.ino
+++ Ejemplos/EnciendeVariosLeds2/EnciendeVariosLeds2.ino
@@ -10,6 +10,9 @@ Enciende un LED por un segundo y lo apaga por el mismo tiempo
 void setup()
 // Se ejecuta cada vez que el Arduino se inicia
 {
+pinMode(2,OUTPUT);
+pinMode(3,OUTPUT);
+pinMode(4,OUTPUT);
 pinMode(5,OUTPUT);
 pinMode(6,OUTPUT);
 pinMode(7,OUTPUT);
@@ -23,25 +26,38 @@ void loop()
 // Esta función se mantiene ejecutando
 {
 // cuando este energizado el Arduino
-digitalWrite(5,HIGH); // Enciende el LED
+digitalWrite(5,LOW); // Enciende el LED
+digitalWrite(5,HIGH);
+// Apaga el LED
 delay(1000);
 // Temporiza un segundo (1s = 1000ms)
+
+digitalWrite(6,LOW); // Enciende el LED
+digitalWrite(6,HIGH);
+// Apaga el LED
+delay(1000);
+// Temporiza un segundo (1s = 1000ms)
+
+digitalWrite(7,LOW); // Enciende el LED
+digitalWrite(7,HIGH);
+// Apaga el LED
+delay(1000);
+// Temporiza un segundo (1s = 1000ms)
+
+
+digitalWrite(5,HIGH); // Enciende el LED
 digitalWrite(5,LOW);
 // Apaga el LED
 delay(1000);
 // Temporiza un segundo (1s = 1000ms)
 
 digitalWrite(6,HIGH); // Enciende el LED
-delay(1000);
-// Temporiza un segundo (1s = 1000ms)
 digitalWrite(6,LOW);
 // Apaga el LED
 delay(1000);
 // Temporiza un segundo (1s = 1000ms)
 
 digitalWrite(7,HIGH); // Enciende el LED
-delay(1000);
-// Temporiza un segundo (1s = 1000ms)
 digitalWrite(7,LOW);
 // Apaga el LED
 delay(1000);
